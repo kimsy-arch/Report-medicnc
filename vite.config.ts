faf7f15388a5cd458cebbb755e83540a63828248
kimsy-arch <kimsy@medicnc.co.kr>
2026-02-19 16:04:43 +0900

    final deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-  base: '/Report-medicnc/',   // ⭐ 여기 추가
+base: '/Report-medicnc/',
 
   server: {
     port: 3000,
