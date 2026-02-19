7a4ac1c9dda67038c2ee95cc9def6814c5cb8a0d
kimsy-arch <kimsy@medicnc.co.kr>
2026-02-19 15:45:46 +0900

    fix: set correct base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
+  base: '/Report-medicnc/',   // ⭐ 여기 추가
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
